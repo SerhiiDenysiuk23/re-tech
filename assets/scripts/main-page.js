b1159e025878af1a741b7cf562ddb7aa44f81b9f
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2024-05-30 14:36:35 +0300

    First Home edits

--- assets/scripts/main-page.js
+++ assets/scripts/main-page.js
@@ -5,6 +5,21 @@ $(document).ready(function () {
     arrows: false,
     infinite: true,
     swipe: true,
-    interval: 500
+    interval: 500,
+    autoplay: true
+  });
+
+  $(".day-product-list-slider").each(function(index, element){
+    var nextArrow = $(this).siblings('.slick-next');
+    var prevArrow = $(this).siblings('.slick-prev');
+
+    $(this).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: nextArrow,
+      prevArrow: prevArrow,
+      autoplay: true,
+      interval: 500
+    });
   });
 })
